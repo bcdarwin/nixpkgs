16d4bb733c5684f6547f5895579b07eb78906a01
Ben Darwin <bcdarwin@gmail.com>
2023-05-10 10:30:56 -0400

    python310Packages.monai-deploy: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6257,6 +6257,8 @@ self: super: with self; {
 
   monai = callPackage ../development/python-modules/monai { };
 
+  monai-deploy = callPackage ../development/python-modules/monai-deploy { };
+
   monero = callPackage ../development/python-modules/monero { };
 
   mongomock = callPackage ../development/python-modules/mongomock { };
