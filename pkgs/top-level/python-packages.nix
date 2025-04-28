fed2bba2084db4b71a7ae8b7ffcb31a2de117c39
Ben Darwin <bcdarwin@gmail.com>
2025-04-28 12:03:16 -0400

    python312Packages.pandarallel: init at 1.6.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11050,6 +11050,8 @@ self: super: with self; {
 
   pandantic = callPackage ../development/python-modules/pandantic { };
 
+  pandarallel = callPackage ../development/python-modules/pandarallel { };
+
   pandas = callPackage ../development/python-modules/pandas { inherit (pkgs.darwin) adv_cmds; };
 
   pandas-datareader = callPackage ../development/python-modules/pandas-datareader { };
